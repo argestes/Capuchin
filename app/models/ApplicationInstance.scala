3c5cc59f1a35672e183ad82be3336096a3518074
Onur Can Çakmak <onur.cakmak@gmail.com>
2013-09-17 23:13:16 -0400

    Update models

--- app/models/ApplicationInstance.scala
+++ app/models/ApplicationInstance.scala
@@ -7,6 +7,7 @@ case class ApplicationInstance(
   id: Option[Guid],
   applicationVersionId: Guid,
   hostId: Guid,
+  data: String,
   status: Int) extends BaseModel
 
 object ApplicationInstances extends BaseTable[ApplicationInstance]("ApplicationInstances") {
@@ -16,9 +17,11 @@ object ApplicationInstances extends BaseTable[ApplicationInstance]("ApplicationI
 
   def hostId = column[Guid]("hostId")
 
+  def data = column[String]("data")
+
   def status = column[Int]("status")
 
-  def * = id.? ~ applicationVersionId ~ hostId ~ status <>(ApplicationInstance, ApplicationInstance.unapply _)
+  def * = id.? ~ applicationVersionId ~ hostId ~ data ~ status <>(ApplicationInstance, ApplicationInstance.unapply _)
 
   def application = foreignKey("FK_ApplicationInstances_Applications", applicationId, Applications)(_.id)
 
--- app/models/ApplicationVersion.scala
+++ app/models/ApplicationVersion.scala
@@ -6,14 +6,17 @@ import play.api.libs.json.{Format, Json}
 case class ApplicationVersion(
   id: Option[Guid],
   applicationId: Guid,
-  name: String) extends BaseModel
+  name: String,
+  url: String) extends BaseModel
 
 object ApplicationVersions extends BaseTable[ApplicationVersion]("ApplicationVersions") {
   def applicationId = column[Guid]("applicationId")
 
   def name = column[String]("name")
 
-  def * = id.? ~ applicationId ~ name <>(ApplicationVersion, ApplicationVersion.unapply _)
+  def url = column[String]("url")
+
+  def * = id.? ~ applicationId ~ name ~ url <>(ApplicationVersion, ApplicationVersion.unapply _)
 
   def application = foreignKey("FK_ApplicationVersions_Applications", applicationId, Applications)(_.id)
 
